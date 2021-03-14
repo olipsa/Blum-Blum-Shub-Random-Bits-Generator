41bf31e6d2a992b3784d3a7025ef2b3a59f61ccf
olipsa <ondina_patricia@yahoo.com>
2021-03-14 22:39:21 +0200

    BBS complete

--- Blum-Blum-Shub.cpp
+++ Blum-Blum-Shub.cpp
@@ -5,10 +5,38 @@ using namespace NTL;
 
 int main()
 {
-	ZZ a, b, c;
+	ZZ p, q;
+	do
+	{
+		RandomPrime(p, 512, 10);
+		while (p % 4 != 3) RandomPrime(p, 512, 10);
+		RandomPrime(q, 512, 10);
+		while (q % 4 != 3) RandomPrime(q, 512, 10);
 
-	cin >> a;
-	cin >> b;
-	c = (a + 1) * (b + 1);
-	cout << c << "\n";
+	}while (GCD((p - 3) / 2, (q - 3) / 2) > 2);
+
+	cout << "p= " << p << endl;
+	cout << "q= " << q << endl;
+	cout << "The gcd is " << GCD((p - 3) / 2, (q - 3) / 2) << endl;;
+	ZZ s(4),M, current(s),previous;
+	int numberOfBits,count0=0,count1=0;
+	cout << "Select the number of random bits to be generated: "; cin >> numberOfBits;
+
+	for (int i = 0; i < numberOfBits; i++)
+		cout << 1 << ' ';
+	cout << endl;
+
+	M = p * q;
+	while (numberOfBits>0)
+	{
+		previous = current;
+		current = power(previous, 2) % M;
+		cout << current % 2 << ' ';
+		current % 2 == 0 ? count0++ : count1++;
+		numberOfBits--;
+	}
+	cout << "\n0 appeared "<<count0<<" times and 1 appeared "<<count1<<" times\n";
+
+
+	
 }
